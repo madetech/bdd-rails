8655b9b49ce3aa582dc5644936812b1f04a426a4
Luke <lukemorton.dev@gmail.com>
2016-04-05 23:13:00 +0100

    Fix bundle install

--- lib/bdd_rails/install_generator.rb
+++ lib/bdd_rails/install_generator.rb
@@ -20,7 +20,33 @@ module BddRails
         gem 'rspec-rails'
       end
 
-      run 'bundle'
+      bundle_command :install
+    end
+
+    private
+
+    def bundle_command(command)
+      say_status :run, "bundle #{command}"
+
+      # We are going to shell out rather than invoking Bundler::CLI.new(command)
+      # because `rails new` loads the Thor gem and on the other hand bundler uses
+      # its own vendored Thor, which could be a different version. Running both
+      # things in the same process is a recipe for a night with paracetamol.
+      #
+      # We unset temporary bundler variables to load proper bundler and Gemfile.
+      #
+      # Thanks to James Tucker for the Gem tricks involved in this call.
+      _bundle_command = Gem.bin_path('bundler', 'bundle')
+
+      require 'bundler'
+      Bundler.with_clean_env do
+        full_command = %Q["#{Gem.ruby}" "#{_bundle_command}" #{command}]
+        if options[:quiet]
+          system(full_command, out: File::NULL)
+        else
+          system(full_command)
+        end
+      end
     end
   end
 end
