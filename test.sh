36603ee0f969fcd70f5aa8c887a798196a807753
Luke <lukemorton.dev@gmail.com>
2016-04-06 20:29:43 +0100

    Add travis test suite

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
-cd example
-rails new --skip-test-unit $1
-cd $1
-echo "gem 'bdd-rails', path: '../../'" >> Gemfile
+set -e
+
+rm -rf test_app
+rails new --skip-test-unit test_app
+cd test_app
+echo "gem 'bdd-rails', path: '../'" >> Gemfile
 bundle
 spring stop
 bundle exec rails g bdd_rails:install
-cd ..
-cd ..
