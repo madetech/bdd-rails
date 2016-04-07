e381267a3887da890a39a39dcdcd21a49c184db5
Luke <lukemorton.dev@gmail.com>
2016-04-07 09:14:11 +0100

    Update spec to use bin version of rails

--- spec/installing_spec.sh
+++ spec/installing_spec.sh
@@ -5,11 +5,11 @@
 set -e
 
 rm -rf test_app
+
 bundle exec rails new --skip-test-unit test_app
 cd test_app
 echo "gem 'bdd-rails', path: '../'" >> Gemfile
 bundle
-cat Gemfile
-bundle exec rails g bdd_rails:install
-bundle exec rake db:migrate
+bin/rails g bdd_rails:install
+bin/rake db:migrate
 bundle exec rspec
